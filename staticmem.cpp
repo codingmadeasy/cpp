334ac46652be1130a2cd2b4a711081ea4f6ddae1
codingmadeasy <110712694+codingmadeasy@users.noreply.github.com>
2023-01-10 15:25:53 +0530

    Update staticmem.cpp

--- staticmem.cpp
+++ staticmem.cpp
@@ -3,18 +3,18 @@ using namespace std;
 class abc
 {
     public:
-    static int a;
+    static int a;//static variable declaration shared by all objects
     static void display()
     {
         cout<<"a = "<<a<<endl;
         a++;
     }
 };
-int abc::a=10;
+int abc::a=10;// definition and initial value
 int main()
 {
     abc ob1,ob2;
-    abc::display();
-    cout<<"a = "<<abc::a;
+    abc::display();//accessing member function using scope resolution operator
+    cout<<"a = "<<abc::a;//accessing data member using scope resolution operator
     return 0;
 }
