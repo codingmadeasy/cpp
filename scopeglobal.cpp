43edec0a7b2adcb229d6b4fc8bf5669830dcfc74
codingmadeasy <110712694+codingmadeasy@users.noreply.github.com>
2023-01-10 15:10:01 +0530

    Update scopeglobal.cpp

--- scopeglobal.cpp
+++ scopeglobal.cpp
@@ -4,6 +4,7 @@ int a=100;// global variable
 int main()
 {
     int a=50;//local variable it will hide global a
-    cout<<"a = "<<a;//it will print value of local a i.e. 50
+    cout<<"a = "<<a;<<endl//it will print value of local a i.e. 50
+    cout<<"a = "<<::a;//it will print value of global a i.e. 100
     return 0;
 }
